8c07968c4613c07efe2b262746c7eb37884a8147
bill-auger <mr.j.spam.me@gmail.com>
2026-02-23 00:22:32 -0500

    version v0.34
    * BPI/BPM voting buttons
    * IRC-style chat timestamp
    * live updates of active jams and jammers

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "LinJam";
     const char* const  companyName    = "";
-    const char* const  versionString  = "0.31.2";
-    const int          versionNumber  = 0x1f02;
+    const char* const  versionString  = "0.34";
+    const int          versionNumber  = 0x2200;
 }
 #endif
